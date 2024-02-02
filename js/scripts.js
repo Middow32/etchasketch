aa3e8f29525b6d6b1db51ff404bdeb62345b0910
Middow32 <mitchellmoore0124@gmail.com>
2024-02-02 12:59:56 -0500

    Removed hovering effect. Color effect changed to random.

--- js/scripts.js
+++ js/scripts.js
@@ -9,14 +9,17 @@ for (i = 0; i < 16; i++) {
     var j = 0;
 
     for (j = 0; j < 16; j++) {
+        
         const column = document.createElement("div");
         column.innerHTML = "";
         column.classList.add("column");
         row.appendChild(column);
-       // column.onmouseover = function() {
-       //     this.style.backgroundColor = "blue";
-       // };
+        column.onmouseover = function() {
+            var randomColor1 = Math.floor(Math.random() * 256);
+            var randomColor2 = Math.floor(Math.random() * 256);
+            var randomColor3 = Math.floor(Math.random() * 256);
+            this.style.backgroundColor = `rgb(${randomColor1},${randomColor2},${randomColor3}`;
+        };
     }
 }
-
 
